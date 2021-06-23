5d23ea0484a3a216a0c7703666f72aa8ea21f953
Scott Gall <scottarthurgall@gmail.com>
2021-06-22 21:04:10 -0500

    add axios

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <template>
   <div>
-    <SearchBar></SearchBar>
+    <SearchBar @termChange="onTermChange"></SearchBar>
   </div>
 </template>
 
 <script>
 import SearchBar from './components/SearchBar';
+const API_KEY = 'AIzaSyCHDAngTB-8IxgaNytIEZ9fR8aAW7a3_-c';
 
 export default {
   name: 'App',
   components: {
     SearchBar
+  },
+  methods: {
+    onTermChange(searchTerm) {
+      console.log(searchTerm);
+    }
   }
 };
 </script>
