7bc798733d1de37e3272bab426fec7b86b4d6538
Scott Gall <scottarthurgall@gmail.com>
2021-06-22 23:15:16 -0500

    added videolistitem

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,40 @@
 <template>
   <div>
     <SearchBar @termChange="onTermChange"></SearchBar>
+    <VideoList :videos="videos"></VideoList>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 import SearchBar from './components/SearchBar';
+import VideoList from './components/VideoList';
 const API_KEY = 'AIzaSyCHDAngTB-8IxgaNytIEZ9fR8aAW7a3_-c';
 
 export default {
   name: 'App',
   components: {
-    SearchBar
+    SearchBar,
+    VideoList
+  },
+  data() {
+    return {
+      videos:[]
+    };
   },
   methods: {
     onTermChange(searchTerm) {
-      console.log(searchTerm);
+      axios.get('https://www.googleapis.com/youtube/v3/search', {
+        params: {
+          key: API_KEY,
+          type: 'video',
+          part: 'snippet',
+          q: searchTerm
+        }
+      }).then(response => {
+        this.videos = response.data.items;
+      });
     }
   }
-};
+}
 </script>
